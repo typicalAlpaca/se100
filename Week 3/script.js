747a09bde6ccd8d6e1f15de7a17ec4f8e21eca20
typicalAlpaca <109391864+typicalAlpaca@users.noreply.github.com>
2024-11-11 19:52:27 +0800

    Week 3 - Exercise 9
    Understanding Operators

--- Week 3/script.js
+++ Week 3/script.js
@@ -1,26 +1,3 @@
-const AAPLCurrentBar = {
-	open: 81.12,
-	high: 82.59,
-	low: 80.09,
-	close: 84.71
-}
-
-const AAPLPrevBar = {
-	open: 81.02,
-	high: 82.49,
-	low: 79.09,
-	close: 83.71
-}
-
-// Write your code below
-let open = (AAPLPrevBar['open'] + AAPLPrevBar['close'])/2;
-console.log(open);
-
-let close = (AAPLCurrentBar['open'] + AAPLCurrentBar['high'] + AAPLCurrentBar['low'] + AAPLCurrentBar['close'])/4;
-console.log(close);
-
-let high = Math.max(AAPLCurrentBar['open'], AAPLCurrentBar['high'], AAPLCurrentBar['close']);
-console.log(high);
-
-let low = Math.min(AAPLCurrentBar['low'], AAPLCurrentBar['open'], AAPLCurrentBar['close']);
-console.log(low);
+(45 - 20) > 4 * 20                      // 25 > 80 is False
+2 * 3 < 4 * 5 || 90 - 45 > 4**3         // (6 < 20) || (45 > 64) is True || False is True
+100 / 10 < (4 + 5) / 2 && !(32 < 20)    // 10 < 4.5 && !(False) is False && True is False
