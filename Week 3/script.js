bd6fa19b868b6de7f46d79c2e6cf50662f0a186d
typicalAlpaca <109391864+typicalAlpaca@users.noreply.github.com>
2024-11-11 19:55:04 +0800

    Week 3 - Exercise 10
    If else-if construct

--- Week 3/script.js
+++ Week 3/script.js
@@ -1,3 +1,13 @@
-(45 - 20) > 4 * 20                      // 25 > 80 is False
-2 * 3 < 4 * 5 || 90 - 45 > 4**3         // (6 < 20) || (45 > 64) is True || False is True
-100 / 10 < (4 + 5) / 2 && !(32 < 20)    // 10 < 4.5 && !(False) is False && True is False
+const stockOHLC = {
+	open: 81.12,
+	high: 82.59,
+	low: 80.09,
+	close: 84.71,
+}
+
+// Write your code below
+if (stockOHLC['open'] > stockOHLC['close']) {
+    console.log("Stock is bearish");
+} else {
+    console.log("Stock is bullish");
+}
