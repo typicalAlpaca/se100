8c8d14682f49fdf1a774861b5394a9d090128f7d
typicalAlpaca <109391864+typicalAlpaca@users.noreply.github.com>
2024-11-11 19:49:05 +0800

    Week 3 - Exercise 8
    Objects

--- Week 3/script.js
+++ Week 3/script.js
@@ -1,8 +1,26 @@
-const TSLAClosingPrices = [230.12, 221.45, 225.61, 219.48, 227.52];
-let avg = 0;
-
-for(let i=0; i<TSLAClosingPrices.length; i++){
-    avg += TSLAClosingPrices[i]/TSLAClosingPrices.length;
+const AAPLCurrentBar = {
+	open: 81.12,
+	high: 82.59,
+	low: 80.09,
+	close: 84.71
 }
 
-console.log(avg);
+const AAPLPrevBar = {
+	open: 81.02,
+	high: 82.49,
+	low: 79.09,
+	close: 83.71
+}
+
+// Write your code below
+let open = (AAPLPrevBar['open'] + AAPLPrevBar['close'])/2;
+console.log(open);
+
+let close = (AAPLCurrentBar['open'] + AAPLCurrentBar['high'] + AAPLCurrentBar['low'] + AAPLCurrentBar['close'])/4;
+console.log(close);
+
+let high = Math.max(AAPLCurrentBar['open'], AAPLCurrentBar['high'], AAPLCurrentBar['close']);
+console.log(high);
+
+let low = Math.min(AAPLCurrentBar['low'], AAPLCurrentBar['open'], AAPLCurrentBar['close']);
+console.log(low);
